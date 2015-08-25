ceafcfb97242013f9a972b8bc36d8ee10535a598
tampointer <nvtam2105@gmail.com>
2015-08-25 17:25:17 +0700

    - process time when add event in file.

--- src/main/java/com/cogivui/controller/EventRestController.java
+++ src/main/java/com/cogivui/controller/EventRestController.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -17,6 +20,7 @@ import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -389,11 +393,76 @@ public class EventRestController {
 		    event.setVenueName(record.get("venue"));
 		    // event.setTime(new Time(record.get("time")));
 		    Time time = new Time();
-		    time.setStartDate(new Date());
-		    time.setEndDate(new Date());
-		    time.setStartPeriod(PeriodTime.AM);
-		    time.setEndPeriod(PeriodTime.PM);
-		    event.setTime(time);
+		    String timeStr = StringUtils.trim(StringUtils.remove(record.get("time"), "\n").replaceAll("\\s+", " "));
+		    if (StringUtils.isNoneBlank(timeStr)) {
+			if (timeStr.contains("Today") || timeStr.contains(" Tomorrow")) {
+			    if (timeStr.contains("Today")) {
+				time.setStartDate(new Date());
+				time.setEndDate(new Date());
+				String[] dateTimeHourMin = StringUtils.split(timeStr, ",");
+				if (dateTimeHourMin.length > 1) {
+				    String[] hoursMin = StringUtils.split(StringUtils.trim(dateTimeHourMin[1]), ":");
+				    if (hoursMin.length > 1) {
+					time.setHourStartDate(Integer.valueOf(hoursMin[0]));
+					time.setMinStartDate(Integer.valueOf(hoursMin[1]));
+					if (Integer.valueOf(hoursMin[0]) >= 12) {
+					    time.setStartPeriod(PeriodTime.PM);
+					} else {
+					    time.setStartPeriod(PeriodTime.AM);
+					}
+				    }
+				}
+			    }
+
+			    if (timeStr.contains("Tomorrow")) {
+				Date tomorrow = DateUtils.addDays(new Date(), 1);
+				time.setStartDate(tomorrow);
+				time.setEndDate(tomorrow);
+				String[] dateTimeHourMin = StringUtils.split(timeStr, ",");
+				if (dateTimeHourMin.length > 1) {
+				    String[] hoursMin = StringUtils.split(StringUtils.trim(dateTimeHourMin[1]), ":");
+				    if (hoursMin.length > 1) {
+					time.setHourStartDate(Integer.valueOf(hoursMin[0]));
+					time.setMinStartDate(Integer.valueOf(hoursMin[1]));
+					if (Integer.valueOf(hoursMin[0]) >= 12) {
+					    time.setStartPeriod(PeriodTime.PM);
+					} else {
+					    time.setStartPeriod(PeriodTime.AM);
+					}
+				    }
+				}
+			    }
+			    event.setTime(time);
+
+			} else {
+			    String[] dateTimeHourMin = StringUtils.split(timeStr, ",");
+			    if (dateTimeHourMin.length > 1) {
+				SimpleDateFormat dt = new SimpleDateFormat("d MMM yyyy");
+				if (StringUtils.split(dateTimeHourMin[1], " ").length <= 2) {
+				    Calendar now = Calendar.getInstance();
+				    int year = now.get(Calendar.YEAR);
+				    dateTimeHourMin[1] = StringUtils.trim(dateTimeHourMin[1]) + " " + year;
+				}
+				Date date = dt.parse(dateTimeHourMin[1]);
+				time.setStartDate(date);
+				time.setEndDate(date);
+				if (dateTimeHourMin.length > 2) {
+				    String[] hoursMin = StringUtils.split(StringUtils.trim(dateTimeHourMin[2]), ":");
+				    if (hoursMin.length > 1) {
+					time.setHourStartDate(Integer.valueOf(hoursMin[0]));
+					time.setMinStartDate(Integer.valueOf(hoursMin[1]));
+					if (Integer.valueOf(hoursMin[0]) >= 12) {
+					    time.setStartPeriod(PeriodTime.PM);
+					} else {
+					    time.setStartPeriod(PeriodTime.AM);
+					}
+				    }
+				}
+			    }
+			    event.setTime(time);
+			}
+
+		    }
 
 		    String des = StringUtils.isBlank(record.get("desc_full")) ? ControllerUtils.br2nl(record.get("desc_full").trim())
 			    : ControllerUtils.br2nl(StringUtils.trim(record.get("desc_short")));
@@ -401,9 +470,9 @@ public class EventRestController {
 
 		    if (StringUtils.isNoneBlank(record.get("latlong"))) {
 			Place place = new Place();
-			String[] longla = StringUtils.trim(record.get("latlong")).split(",");
-			place.setLatitude(longla[0]);
-			place.setLongitude(longla[1]);
+			String[] lalong = StringUtils.trim(record.get("latlong")).split(",");
+			place.setLatitude(lalong[0]);
+			place.setLongitude(lalong[1]);
 			try {
 			    GoogleResponse res1 = new AddressConverter().convertFromLatLong(StringUtils.trim(record.get("latlong")));
 			    if ("OK".equals(res1.getStatus()) && res1.getResults().length > 0) {
@@ -421,13 +490,13 @@ public class EventRestController {
 			}
 
 			if (place.getAddress().contains(URLDecoder.decode("H%E1%BB%93+Ch%C3%AD+Minh", "UTF-8"))) {
-			    place.setCityId(1);
+			    place.setCityId(1); // HCM
 			} else if (place.getAddress().contains(URLDecoder.decode("H%C3%A0+N%E1%BB%99i", "UTF-8"))) {
-			    place.setCityId(2);
+			    place.setCityId(2);// HN
 			} else if (place.getAddress().contains(URLDecoder.decode("%C4%90%C3%A0+N%E1%BA%B5ng", "UTF-8"))) {
-			    place.setCityId(3);
+			    place.setCityId(3);// DN
 			} else {
-			    place.setCityId(0);
+			    place.setCityId(0);// None
 			}
 			event.setPlace(place);
 		    }
@@ -441,9 +510,6 @@ public class EventRestController {
 		    // city ==> send long, lat to Google ==> extract
 		    // category, hotline, price, time
 		    list.add(event);
-		    // if (i/10 == 0) {
-		    // Thread.sleep(2000);
-		    // }
 		    System.out.println(record);
 		}
 		eventService.saveOrUpdate(list);
@@ -461,6 +527,18 @@ public class EventRestController {
 	    }
 	} else {
 	    return new Status(0, "You failed to upload " + name + " because the file was empty.");
+	}
+    }
+
+    public static void main(String[] args) {
+	String oldstring = "16 December 2015";
+	SimpleDateFormat dt = new SimpleDateFormat("d MMM yyyy");
+	try {
+	    Date date = dt.parse(oldstring);
+	    System.out.println(date);
+	} catch (ParseException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
     }
 
